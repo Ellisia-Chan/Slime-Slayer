8b8569af8c3e6beab6d102d2ebb6798b9c7130e3
Ellisia <christianjudevillaber@gmail.com>
2024-07-29 12:30:18 +0800

    animation Scripts

--- Scripts/Animations/Weapon/SwordAnimator.cs
+++ Scripts/Animations/Weapon/SwordAnimator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,17 @@ public class SwordAnimator : MonoBehaviour {
 
     private const string ATTACK_TRIGGER = "Attack";
 
+    [SerializeField] private float swordCD = 0.5f;
+
     private Animator swordAnimator;
+    private Transform swordCollider;
     private Vector3 mousePos;
     private Vector3 playerTransformPosition;
     private Vector3 playerScreenPoint;
     private float angleRotation;
+    private bool attackButtondDown = false;
+    private bool isAttacking = false;
+
 
     private void Awake() {
         Instance = this;
@@ -21,33 +28,78 @@ public class SwordAnimator : MonoBehaviour {
 
     private void Start() {
         GameInput.Instance.OnAttackAction += GameInput_OnAttackAction;
+        GameInput.Instance.OnAttackActionCancel += GameInput_OnAttackActionCancel;
     }
 
-    private void OnDestroy() {
+    private void OnDisable() {
         GameInput.Instance.OnAttackAction -= GameInput_OnAttackAction;
+        GameInput.Instance.OnAttackActionCancel -= GameInput_OnAttackActionCancel;
     }
 
     private void GameInput_OnAttackAction(object sender, System.EventArgs e) {
-        swordAnimator.SetTrigger(ATTACK_TRIGGER);
-        SwordSlashAnimator.Instance.SpawnSwordSlash();
+        IsAttacking();
+    }
+
+    private void GameInput_OnAttackActionCancel(object sender, EventArgs e) {
+        IsDoneAttacking();
     }
 
     private void Update() {
         SwordFollowMouseOffset();
+        Attack();
     }
+
+    private void IsAttacking() {
+        attackButtondDown = true;
+    }
+
+    private void IsDoneAttacking() {
+        attackButtondDown = false;
+    }
+
+    private void Attack() {
+        if (attackButtondDown && !isAttacking) {
+            isAttacking = true;
+            swordAnimator.SetTrigger(ATTACK_TRIGGER);
+            SwordSlashAnimator.Instance.SpawnSwordSlash();
+            StartCoroutine(AttackCDRoutine());
+        }
+    }
+
+    private IEnumerator AttackCDRoutine() {
+        yield return new WaitForSeconds(swordCD);
+        isAttacking = false;
+    }
+
 
     private void SwordFollowMouseOffset() {
         mousePos = PlayerController.Instance.GetMousePosition();
         playerTransformPosition = PlayerController.Instance.GetTransformPosition();
+        swordCollider = Sword.Instance.GetSwordCollider();
 
         playerScreenPoint = Camera.main.WorldToScreenPoint(playerTransformPosition);
         angleRotation = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
         if (mousePos.x < playerScreenPoint.x) {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180, angleRotation);
+            swordCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
         } else {
             ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angleRotation);
-
+            swordCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
+
+    public void OnSwordAnimationEnd() {
+        Sword.Instance.DisableSwordCollider();
+    }
+
+    public bool IsAttackingPerform() {
+        return isAttacking;
+    }
+
+    public bool IsAttackButtonDown() {
+        return attackButtondDown;
+    }
+
+
 }
