58d35c00288a75083133f751e99715475a33c9f5
Ellisia <christianjudevillaber@gmail.com>
2024-07-30 13:35:34 +0800

    GameInput Scripts

--- Scripts/GameInput/GameInput.cs
+++ Scripts/GameInput/GameInput.cs
@@ -9,6 +9,7 @@ public class GameInput : MonoBehaviour {
 
     public event EventHandler OnAttackAction;
     public event EventHandler OnAttackActionCancel;
+    public event EventHandler OnDashAction;
 
     public PlayerInputActions playerInputActions;
 
@@ -28,6 +29,13 @@ public class GameInput : MonoBehaviour {
     private void Start() {
         playerInputActions.Player.Attack.performed += Attack_performed;
         playerInputActions.Player.Attack.canceled += Attack_canceled;
+        playerInputActions.Player.Dash.performed += Dash_performed;
+    }
+
+    private void OnDestroy() {
+        playerInputActions.Player.Attack.performed -= Attack_performed;
+        playerInputActions.Player.Attack.canceled -= Attack_canceled;
+        playerInputActions.Player.Dash.performed -= Dash_performed;
     }
 
     private void Attack_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
@@ -36,6 +44,10 @@ public class GameInput : MonoBehaviour {
 
     private void Attack_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
         OnAttackAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Dash_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnDashAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized() {
